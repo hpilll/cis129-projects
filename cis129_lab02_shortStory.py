ccadfa717a161da06da8ce9f3ecf8d8dd596e475
hpilll <hpiller@mail.pima.edu>
2024-09-21 19:47:22 -0700

    Update cis129_lab02_shortStory.py
    Added three print statements to tell my story.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,8 @@
-print('Hello, World!')
+#Author: Halianna Piller
+#My first Python lab in my CIS129 course
+#Prints a simple short story
+
+print("There was once a little black cat name Bella. She was a stray living in an apartment complex.")
+print("One night a couple sat on their chair outside their apartment. The little black cat jumped on their laps.")
+print("That night, the couple took in the cat and it lived with the couple the rest of her life.") 
+      
